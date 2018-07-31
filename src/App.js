1da29c226eda007900293afea619542953ebc26b
Ryan <ryannaylor@hotmail.com>
2018-07-31 21:02:40 +0100

    Replace default React items with more specific values

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,11 +6,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Fantasy Stats</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          A collection of statistics based on the Barclays Fantasty Football site
         </p>
       </div>
     );
