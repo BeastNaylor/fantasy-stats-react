96b511c0d7e820860c35b31db1b72d471791bb4e
Ryan Naylor <ryan.naylor@hs2.org.uk>
2018-08-03 11:50:48 +0100

    Added a test for the Routes component

---
+++ src/routes.test.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { mount, shallow } from "enzyme";
+
+import App from "./App";
+import Home from "./components/home/home";
+import League from "./components/league/league";
+import NotFound from "./components/notfound/notfound";
+import { MemoryRouter } from "react-router";
+import Routes from "./routes";
+
+const renderRoutes = path =>
+  mount(
+    <MemoryRouter initialEntries={[path]}>
+      <Routes />
+    </MemoryRouter>
+  );
+describe("#routes", () => {
+  it("renders home page on initial route", () => {
+    const component = renderRoutes("/");
+
+    expect(component.find(Home)).toHaveLength(1);
+  });
+  it("renders about page for league", () => {
+    const component = renderRoutes("/league");
+
+    expect(component.find(League)).toHaveLength(1);
+  });
+  it("renders notfound page for bad url", () => {
+    const component = renderRoutes("/randomurl");
+
+    expect(component.find(NotFound)).toHaveLength(1);
+  });
+});
