780bf24e5bd1b5afc2aa43308aa15c8287322667
Ryan Naylor <ryan.naylor@hs2.org.uk>
2018-08-03 11:33:43 +0100

    Set up basic enzyme test to check the suite can run correctly.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {mount} from 'enzyme'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App><h1>Hello</h1></App>, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+
+it('works with enzyme', () => {
+  let props = {
+    "children" : <h1>Hello</h1>
+  }
+  mount(<App {...props}/>);
 });
