ea897e42abe88639ba2d94b93dc95f7cae36ad07
Ryan Naylor <ryan.naylor@hs2.org.uk>
2018-08-08 10:11:17 +0100

    Added a constants folder for simple config

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,12 @@ import App from './components/App'
 import Home from './components/home/home'
 import League from './components/league/league'
 import NotFound from './components/notfound/notfound'
+import {DEFAULT_LEAGUE_CODE} from './constants/codes'
 
 const Routes = () => (
   <App>
     <Switch>
-      <Route exact path="/" render={() => <Home code="456"/>} />;
+      <Route exact path="/" render={() => <Home code={DEFAULT_LEAGUE_CODE}/>} />;
       <Route path="/league/:code" component={League} />
       <Route path="*" component={NotFound} />
     </Switch>
